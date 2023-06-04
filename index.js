a0dac7fad369e6b5fe66891534777f504d4cbe61
raj <rk7889666@gmail.com>
2023-06-04 18:09:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";//const express=require("express"); module import 
+import cors from 'cors';//we can run fe and be running on diff ports
+import Connection from "./database/db.js";
+import Routes  from './routes/route.js'
+import bodyParser from 'body-parser'//helper pkg of express
+
+const app=express(); //initialize and returns objects that has lots of usefull methods
+app.use(cors());
+
+app.use(bodyParser.json({extended:true}))//app.use(express.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/',Routes)//first parse url and then call route
+// const PORT=8000;//for development
+const PORT=process.env.PORT||8000;//for deployment
+
+Connection()
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`)
+});
+
